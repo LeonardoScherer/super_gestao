b529ec707e275603f1b240651252f019a0d42435
LeonardoScherer <leoblacena11@gmail.com>
2022-06-11 18:05:24 -0300

    Implementando rotas principais

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sobre-nos', function () {
+    return view('sobre-nos');
+});
+
+Route::get('/contato', function () {
+    return view('contato');
+});
