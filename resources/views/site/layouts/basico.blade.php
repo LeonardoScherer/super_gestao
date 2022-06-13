619d4c99c146eb65d1ae88393a53bb620acb846f
LeonardoScherer <leoblacena11@gmail.com>
2022-06-12 23:57:09 -0300

    Implementando include do menu

--- resources/views/site/layouts/basico.blade.php
+++ resources/views/site/layouts/basico.blade.php
@@ -6,6 +6,7 @@
         <link rel="stylesheet" href="{{ asset('css/style_basic.css') }}">
     </head>
     <body>
+        @include('site.layouts._partials.topo')
         @yield('conteudo')
     </body>
 </html>
